58824368f638283e7f2684fad457261bcd828dd7
GeorgeH14 <160240028@student.rnngroup.ac.uk>
2026-01-13 20:10:41 +0000

    Revert "Refactored password generator. Added comments for better clarity."
    This reverts commit 5c4cac29e7f330ab1849bafa3fa08d93a02b5190.

--- password_generator.py
+++ password_generator.py
@@ -7,10 +7,10 @@ def password_gen():
   pass_chars = 0
 
   random_list = []
-  x = input('How many characters should your password be?') #Allows user to select length of their password.
-  x = int(x) # Converts to int as input will always be registered as string, will cause ValueError if not changed.
+  x = input('How many characters should your password be?')
+  x = int(x)
 
-  while pass_chars <x: #Follows user input to create the correct length of password.
+  while pass_chars <x:
     random_char = random.choice(chars)
     random_specs = random.choice(spec_chars)
     random_i = random.randint(0,9)
