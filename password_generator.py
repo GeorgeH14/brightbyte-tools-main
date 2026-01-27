ae3809ca9c18b9e5730e384df3be1b4dd03a150d
GeorgeH14 <160240028@student.rnngroup.ac.uk>
2026-01-27 19:48:00 +0000

    Added CLI to password generator.

--- password_generator.py
+++ password_generator.py
@@ -1,14 +1,15 @@
 #Simple password generator
+# New feature that now uses cli
 import random
+import sys
 
-def password_gen():
+def password_gen(x=0):
   spec_chars = ["!","£","$","%","^","&","*",".","/",":","@","~","#","_","-","=","+","?"]
   chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   pass_chars = 0
 
   random_list = []
-  x = input('How many characters should your password be?')
-  x = int(x)
+ 
 
   while pass_chars <x:
     random_char = random.choice(chars)
@@ -21,6 +22,17 @@ def password_gen():
 
   final_password = "".join(random_list)
 
-  print(f"Your password is:\n {final_password}")
+  #print(f"Your password is:\n {final_password}")
+  return final_password
 
-password_gen()
+if len(sys.argv) < 2:
+  print('Welcome to my Password Generator.\n')
+  x = int(input("Length: "))
+else:
+  x = int(sys.argv[1])
+
+cli_pass = password_gen(x)
+print(f'Your password is {cli_pass}')
+
+
+
